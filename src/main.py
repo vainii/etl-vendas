4f83f2daed01265f5cbc21facbb39539ecca237a
Guilherme Vaini <guilhermevaini10@gmail.com>
2026-04-18 17:04:45 -0300

    fix: corrige caminhos dos arquivos na pipeline

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from transform import transform_data
 from load import load_data
 
 def run_pipeline():
-    df = extract_data('../data/vendas_raw.csv')
+    df = extract_data('data/vendas_raw.csv')
     df = transform_data(df)
-    load_data(df, '../data/vendas_tratado.csv')
+    load_data(df, 'data/vendas_tratado.csv')
 
     print("Pipeline executado com sucesso!")
 
